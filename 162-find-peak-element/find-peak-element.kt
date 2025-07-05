5947d9a878b65ef86df1e935e2d9a66a47a31714
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-05 16:33:42 +0300

    Time: 0 ms (100.00%) | Memory: 42.8 MB (78.15%) - LeetSync

--- 162-find-peak-element/find-peak-element.kt
+++ 162-find-peak-element/find-peak-element.kt
@@ -4,21 +4,15 @@ class Solution {
         var l = 0
         var r = nums.size - 1
 
-        while(l <= r) {
-            
-            val leftL = if (l - 1 >= 0) nums[l - 1] else Int.MIN_VALUE
-            val rightL = if (l + 1 < nums.size) nums[l + 1] else Int.MIN_VALUE
-
-            val leftR = if (r - 1 >= 0) nums[r - 1] else Int.MIN_VALUE
-            val rightR = if (r + 1 < nums.size) nums[r + 1] else Int.MIN_VALUE
-
-            if (nums[l] > leftL && nums[l] > rightL) return l
-            if (nums[r] > leftR && nums[r] > rightR) return r
-
-            l++
-            r--
+        while(l < r) {  
+            val mid = l + (r - l) / 2
+            if (nums[mid] > nums[mid + 1]) {
+                r = mid
+            } else {
+                l = mid + 1
+            }
         }
 
-        return 0
+        return l
     }
 }
