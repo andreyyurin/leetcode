620b7d97930cf95ff85102b609907d5c6563f1d2
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-07 23:47:57 +0300

    Time: 109 ms (36.84%) | Memory: 96.4 MB (42.10%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.kt
@@ -0,0 +1,44 @@
+class Solution {
+    // Чужой код, если вернешься сюда - пиши свой!
+    fun maxEvents(events: Array<IntArray>): Int {
+        events.sortBy { it[0] }
+        val queue = PriorityQueue<IntArray>(compareBy { it[1] })
+        var ans = 0
+        val minDay = events[0][0]
+        val maxDay = events.last()[1]
+        var day = minDay
+        var i = 0
+
+        fun enqueue() {
+            while (i < events.size && events[i][0] <= day) {
+                queue.add(events[i])
+                i++
+            }
+        }
+
+        fun dequeue() {
+            while (queue.isNotEmpty()) {
+                val event = queue.poll()
+                if (day <= event[1]) {
+                    ans++
+                    day++
+                    enqueue()
+                }
+            }
+        }
+
+        enqueue()
+
+        while (i < events.size && day <= maxDay) {
+            dequeue()
+            if (i < events.size) {
+                day = max(day, events[i][0])
+                enqueue()
+            }
+        }
+
+        dequeue()
+
+        return ans
+    }
+}
