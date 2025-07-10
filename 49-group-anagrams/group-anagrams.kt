1263bc6e5607fce3c09039efc98338a7051588ba
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-10 23:15:19 +0300

    Time: 16 ms (94.57%) | Memory: 50 MB (99.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.kt
+++ 49-group-anagrams/group-anagrams.kt
@@ -2,18 +2,19 @@ class Solution {
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
         val map = mutableMapOf<Int, Int>() 
         val result = LinkedList<LinkedList<String>>()
-        var mult = IntArray(26,{1})
+        var charPrime = IntArray(26) { 1 }
         val prime = 101
 
         for (i in 1 until 26) {
-            mult[i] = mult[i-1]*prime
+            charPrime[i] = charPrime[i-1] * prime
         }
 
         var counter = 0
+
         for(i in strs) {
             var sum = 0
             for (j in i) {
-                sum += mult[(j - 'a')]
+                sum += charPrime[(j - 'a')]
             }
             if (map[sum] == null) {
                 result.add(LinkedList(listOf(i)))
