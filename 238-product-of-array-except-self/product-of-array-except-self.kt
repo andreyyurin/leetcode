3801724b5a78cf787963ec05a1892520df073d4b
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-11 13:08:41 +0300

    Time: 17 ms (15.66%) | Memory: 65.3 MB (5.42%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        var point = 1
+        var counterZero = 0
+        var counterMinus = 0
+        nums.forEach {
+            if (it == 0) {
+                counterZero++
+            } else {
+                point *= it
+            }
+            if (it < 0) {
+                counterMinus++
+            }
+        }
+
+        val list = mutableListOf<Int>()
+
+        nums.forEach {
+            if(it == 0 && counterZero > 1) {
+                list.add(0)
+            } else if (it == 0 && counterZero == 1) {
+                list.add(point)
+            } else if(counterZero > 0) {
+                list.add(0)
+            } else if (it < 0) {
+                if (counterMinus - 1 % 2 == 0) {
+                    list.add(point / it)
+                } else {
+                    list.add(point / it)
+                }
+            } else {
+                list.add(point / it)
+            }
+        }
+        
+        return list.toIntArray()
+    }
+}
