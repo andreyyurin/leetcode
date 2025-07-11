47f67522569edc0766c4875af21b573cb08c2474
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-11 13:13:34 +0300

    Time: 4 ms (42.05%) | Memory: 60.6 MB (73.73%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.kt
+++ 238-product-of-array-except-self/product-of-array-except-self.kt
@@ -16,22 +16,62 @@ class Solution {
 
         val list = IntArray(nums.size)
 
-        nums.forEachIndexed { index, it ->
-            if(it == 0 && counterZero > 1) {
-                list[index] = (0)
-            } else if (it == 0 && counterZero == 1) {
-                list[index] = (point)
-            } else if(counterZero > 0) {
-                list[index] = (0)
-            } else if (it < 0) {
-                if (counterMinus - 1 % 2 == 0) {
-                    list[index] = (point / it)
+        var l = 0
+        var r = nums.size - 1
+        while(l <= r) {
+            if (l == r) {
+                if(nums[l] == 0 && counterZero > 1) {
+                    list[l] = (0)
+                } else if (nums[l] == 0 && counterZero == 1) {
+                    list[l] = (point)
+                } else if(counterZero > 0) {
+                    list[l] = (0)
+                } else if (nums[l] < 0) {
+                    if (counterMinus - 1 % 2 == 0) {
+                        list[l] = (point / nums[l])
+                    } else {
+                        list[l] = (point / nums[l])
+                    }
                 } else {
-                    list[index] = (point / it)
+                    list[l] = point / nums[l]
                 }
             } else {
-                list[index] = point / it
+                if(nums[r] == 0 && counterZero > 1) {
+                    list[r] = (0)
+                } else if (nums[r] == 0 && counterZero == 1) {
+                    list[r] = (point)
+                } else if(counterZero > 0) {
+                    list[r] = (0)
+                } else if (nums[r] < 0) {
+                    if (counterMinus - 1 % 2 == 0) {
+                        list[r] = (point / nums[r])
+                    } else {
+                        list[r] = (point / nums[r])
+                    }
+                } else {
+                    list[r] = point / nums[r]
+                }
+
+                if(nums[l] == 0 && counterZero > 1) {
+                    list[l] = (0)
+                } else if (nums[l] == 0 && counterZero == 1) {
+                    list[l] = (point)
+                } else if(counterZero > 0) {
+                    list[l] = (0)
+                } else if (nums[l] < 0) {
+                    if (counterMinus - 1 % 2 == 0) {
+                        list[l] = (point / nums[l])
+                    } else {
+                        list[l] = (point / nums[l])
+                    }
+                } else {
+                    list[l] = point / nums[l]
+                }
             }
+
+
+            l++
+            r--
         }
         
         return list
