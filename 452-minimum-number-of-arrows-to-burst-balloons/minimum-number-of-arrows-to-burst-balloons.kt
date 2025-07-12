3e5c07e1c34760e3cb710e6ee2b687365db617b8
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-12 13:41:21 +0300

    Time: 148 ms (64.91%) | Memory: 95.1 MB (85.38%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.kt
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.kt
@@ -2,15 +2,12 @@ class Solution {
     fun findMinArrowShots(points: Array<IntArray>): Int {
         points.sortBy { it[1] }
 
-        val tempList = mutableListOf<IntArray>()
-        //println(points.map {"${it[0]} ${it[1]}"})
-
-        tempList.add(points[0])
+        var start = points[0][1]
 
         var counter = 1
         for (i in 1 until points.size) {
-            if (points[i][0] > tempList[counter - 1][1]) {
-                tempList.add(intArrayOf(points[i][0], points[i][1]))
+            if (points[i][0] > start) {
+                start = points[i][1]
                 counter++
             }
         }
