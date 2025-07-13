864693549bab1c61116fff8d1dd85382fdedfc51
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-13 11:20:47 +0300

    Time: 51 ms (50.00%) | Memory: 64.3 MB (62.50%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.kt
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.kt
@@ -18,46 +18,5 @@ class Solution {
             }
         }
         return result
-        // val maxTrainer = trainers.last()
-        // var counter = 0
-
-        // var lPlayer = 0
-        // var rPlayer = players.size - 1
-
-        // println(players.map { it })
-        // println(trainers.map { it })
-        // var result = 0
-        // var lTrainer = 0
-        // var rTrainer = trainers.size - 1
-        // var map = mutableMapOf<Int, Boolean>()
-        // while(lPlayer <= rPlayer) {
-        //     if(lTrainer > rTrainer) break
-
-        //     if (players[lPlayer] > trainers[lTrainer]) {
-        //         lTrainer++
-        //     } else {
-        //         lPlayer++
-        //         lTrainer++
-        //         if (map[lTrainer] != true) {
-        //             result++
-        //             map[lTrainer] = true
-        //         }
-        //     }
-
-        //     if (players[rPlayer] > trainers[rTrainer]) {
-        //         rPlayer--
-        //     } else {
-        //         rPlayer--
-        //         rTrainer--
-
-        //         if (map[rTrainer] != true && lTrainer < rTrainer) {
-        //             result++
-        //             map[rTrainer] = true
-        //         }
-        //     }
-
-        // }
-
-        return result
     }
 }
