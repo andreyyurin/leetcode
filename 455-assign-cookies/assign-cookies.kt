45e53e002b2bfbb4f31f2b0806b3de15d79277b0
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-14 12:27:31 +0300

    Time: 22 ms (63.44%) | Memory: 53.6 MB (52.69%) - LeetSync

--- 455-assign-cookies/assign-cookies.kt
+++ 455-assign-cookies/assign-cookies.kt
@@ -2,8 +2,6 @@ class Solution {
     fun findContentChildren(g: IntArray, s: IntArray): Int {
         g.sort()
         s.sort()
-        println(g.map{it})
-        println(s.map{it})
         var j = 0
         var k = 0
         var i = 0
