9136bb4a7f260989f51fcffe658c77e329a33335
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-15 12:47:02 +0300

    Time: 17 ms (23.68%) | Memory: 63.7 MB (28.95%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.kt
+++ 2634-minimum-common-value/minimum-common-value.kt
@@ -4,8 +4,7 @@ class Solution {
         nums2.sort()
         var j = 0
         var i = 0
-        println(nums1.map { it })
-        println(nums2.map { it })
+        
         while(true) {
             if (j >= nums2.size || i >= nums1.size) break
             if (nums1[i] == nums2[j]) {
