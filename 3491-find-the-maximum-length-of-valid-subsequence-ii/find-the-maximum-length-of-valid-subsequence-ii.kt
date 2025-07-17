0c1e3168329e4580acaad996e14cab06dbf50e02
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-17 14:39:13 +0300

    Time: 53 ms (100.00%) | Memory: 57.9 MB (100.00%) - LeetSync

---
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun maximumLength(nums: IntArray, k: Int): Int {
+        val n = nums.size
+        var answer = 0
+        val cache = Array(k) { IntArray(n) { 1 } }
+        for (i in 1..<n) {
+            for (j in 0..<i) {
+                val mod = (nums[i] + nums[j]) % k
+                cache[mod][i] = max(cache[mod][i], cache[mod][j] + 1)
+                answer = max(answer, cache[mod][i])
+            }
+        }
+        return answer
+    }
+}
