f5a275fcd887a88fcca789695f76754a36427266
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-22 14:38:20 +0300

    Time: 2 ms (38.10%) | Memory: 44.5 MB (49.52%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.kt
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.kt
@@ -11,7 +11,7 @@
 class Solution {
     val root: TreeNode? = null
     lateinit var nums: IntArray
-    val added = mutableListOf<Int>()
+    val added = mutableMapOf<Int, Boolean>()
     fun sortedArrayToBST(nums: IntArray): TreeNode? {
         this.nums = nums
         val result = go(0, nums.size, -1)
@@ -20,9 +20,9 @@ class Solution {
 
     fun go(start: Int, end: Int, prevMid: Int): TreeNode? {
         val mid = (start + end) / 2
-        if (prevMid == mid || start >= end || mid in added) return null
+        if (prevMid == mid || start >= end || added[mid] != null) return null
         val root = TreeNode(nums[mid])
-        added.add(mid)
+        added[mid] = true
 
         root.right = go(mid, end, mid)
         root.left = go(start, mid, mid)
