d07ab1273a04d1f660e7b46c84dee4610476251b
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-24 00:29:03 +0300

    Time: 112 ms (81.82%) | Memory: 40 MB (95.45%) - LeetSync

---
+++ 1656-count-good-triplets/count-good-triplets.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
+        var ans = 0
+        for(i in 0 until arr.size) {
+            for (j in i + 1 until arr.size) {
+                for (k in j + 1 until arr.size) {
+                    if(abs(arr[i] - arr[j]) <= a
+                    && abs(arr[j] - arr[k]) <= b
+                    && abs(arr[i] - arr[k]) <= c
+                    ) {
+                        ans++
+                    }
+                }
+            }
+        }
+        return ans
+    }
+}
