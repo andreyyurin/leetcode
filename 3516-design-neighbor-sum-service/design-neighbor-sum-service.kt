e78bf47cf0c34df3f223e6b9b68de7969cbdcc07
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-25 12:37:47 +0300

    Time: 44 ms (83.33%) | Memory: 58.1 MB (50.00%) - LeetSync

--- 3516-design-neighbor-sum-service/design-neighbor-sum-service.kt
+++ 3516-design-neighbor-sum-service/design-neighbor-sum-service.kt
@@ -1,83 +1,52 @@
 class NeighborSum(private val grid: Array<IntArray>) {
     var sum = 0
 
-    fun adjacentSum(value: Int): Int {
-        sum = 0
+    private val map = mutableMapOf<Int, Pair<Int, Int>>()
+    init {
         for (i in 0 until grid.size) {
             for (j in 0 until grid[i].size) {
-                if (grid[i][j] == value) {
-                    adj(i - 1, j, Direction.top)
-                    adj(i + 1, j, Direction.bottom)
-                    adj(i, j - 1, Direction.left)
-                    adj(i, j + 1, Direction.right)
-                    
-                    return sum
-                }
+                map[grid[i][j]] = i to j
             }
         }
-        return 0
+    }
+
+    fun adjacentSum(value: Int): Int {
+        sum = 0
+
+        val (i, j) = map[value]!!
+
+        adj(i - 1, j)
+        adj(i + 1, j)
+        adj(i, j - 1)
+        adj(i, j + 1)
+                    
+        return sum
     }
 
     fun diagonalSum(value: Int): Int {
         sum = 0
-        for (i in 0 until grid.size) {
-            for (j in 0 until grid[i].size) {
-                if (grid[i][j] == value) {
-                    dia(i + 1, j + 1, Direction.top)
-                    dia(i + 1, j - 1, Direction.bottom)
-                    dia(i - 1, j + 1, Direction.left)
-                    dia(i - 1, j - 1, Direction.right)
-                    return sum
-                }
-            }
-        }
-        return 0
+
+        val (i, j) = map[value]!!
+
+        dia(i + 1, j + 1)
+        dia(i + 1, j - 1)
+        dia(i - 1, j + 1)
+        dia(i - 1, j - 1)
+
+        return sum
     }
 
-    private fun adj(i: Int, j: Int, dir: Direction) {
+    private fun adj(i: Int, j: Int) {
         if (i < 0 || i >= grid.size) return
         if (j < 0 || j >= grid[i].size) return
         sum += grid[i][j]
-        // when(dir) {
-        //     Direction.top -> {
-        //         adj(i - 1, j, dir)
-        //     }
-        //     Direction.bottom -> {
-        //         adj(i + 1, j, dir)
-        //     }
-        //     Direction.left -> {
-        //         adj(i, j - 1, dir)
-        //     }
-        //     Direction.right -> {
-        //         adj(i, j + 1, dir)
-        //     }
-        //}
     }
 
-    private fun dia(i: Int, j: Int, dir: Direction) {
+    private fun dia(i: Int, j: Int) {
         if (i < 0 || i >= grid.size) return
         if (j < 0 || j >= grid[i].size) return
         sum += grid[i][j]
-        // when(dir) {
-        //     Direction.top -> {
-        //         dia(i - 1, j + 1, dir)
-        //     }
-        //     Direction.bottom -> {
-        //         dia(i + 1, j + 1, dir)
-        //     }
-        //     Direction.left -> {
-        //         dia(i - 1, j - 1, dir)
-        //     }
-        //     Direction.right -> {
-        //         dia(i + 1, j - 1, dir)
-        //     }
-        // }
     }
-
-    enum class Direction {
-        top, bottom, left, right;
-    }
-
 }
 
 /**
