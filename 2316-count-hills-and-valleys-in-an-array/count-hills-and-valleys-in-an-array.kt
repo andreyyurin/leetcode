ff934ca3100891ea6c2ca1542f45f842dd0f6e9d
Andrey Yurin <andrey.yurin55@gmail.com>
2025-07-27 14:06:15 +0300

    Time: 10 ms (0.00%) | Memory: 43.1 MB (20.00%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.kt
@@ -0,0 +1,36 @@
+class Solution {
+    fun countHillValley(nums: IntArray): Int {
+        val num = mutableListOf<Int>()
+
+        for (i in 0 until nums.size) {
+            if (i > 0 && nums[i] != nums[i - 1]) {
+                num.add(nums[i])
+            } else if (i == 0) {
+                num.add(nums[i])
+            }
+        }
+        println(num)
+        val hill = IntArray(num.size) { 0 }
+
+        for (i in 1 until num.size - 1) {
+            if (num[i] > num[i - 1] && num[i] > num[i + 1]) {
+                hill[i] = 1 // hill
+            } else if (num[i] < num[i - 1] && num[i] < num[i + 1]) {
+                hill[i] = 2 // valley
+            } else if (num[i] == num[i - 1] && num[i] < num[i + 1] && hill[i - 1] == 2) {
+                hill[i] = 2
+            } else if (num[i] == num[i - 1] && num[i] > num[i + 1] && hill[i - 1] == 1) {
+                hill[i] = 1
+            }
+        }
+
+        var res = 0
+        println(hill.map { it })
+        for(i in 1 until num.size - 1) {
+            if (hill[i] != hill[i - 1] && hill[i] != 0) {
+                res ++
+            }
+        }
+        return res
+    }
+}
