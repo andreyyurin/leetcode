7f00fbb29ab5a97bdac8d2facec56d77cbbdeec0
Andrey Yurin <andrey.yurin55@gmail.com>
2025-08-12 11:29:09 +0300

    Time: 13 ms (85.71%) | Memory: 42.7 MB (100.00%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.kt
@@ -0,0 +1,64 @@
+class Solution {
+    fun numberOfWays(
+        n: Int,
+        x: Int,
+    ): Int {
+        if (x == 1) {
+            return numberOfWaysMod(n)
+        }
+
+        // dp[sum]@num:= the number of ways to achieve sum, using
+        // only numbers less than or equal to num.
+        val dp = IntArray(n + 1)
+        dp[0] = 1
+        for (num in 1..n) {
+            val powerX = pow(num, x)
+            if (n < powerX) {
+                break
+            }
+
+            for (sum in n downTo powerX) {
+                dp[sum] += dp[sum - powerX]
+            }
+        }
+        return dp[n]
+    }
+
+    // Computes numberOfWays(n, 1).
+    private fun numberOfWaysMod(n: Int): Int {
+        val modulo = 1_000_000_007
+
+        // dp[sum]@num:= the number of ways to achieve sum, taken
+        // modulo, using only numbers less than or equal to num.
+        val dp = IntArray(n + 1)
+        dp[0] = 1
+        for (num in 1..n) {
+            for (sum in n downTo num) {
+                dp[sum] += dp[sum - num]
+                if (dp[sum] > modulo) {
+                    dp[sum] -= modulo
+                }
+            }
+        }
+        return dp[n]
+    }
+
+    // Returns base^exp.
+    private fun pow(
+        base: Int,
+        exp: Int,
+    ): Int {
+        var result = 1
+        var newBase = base
+        var newExp = exp
+
+        while (newExp > 0) {
+            if (newExp and 1 == 1) {
+                result *= newBase
+            }
+            newBase *= newBase
+            newExp = newExp shr 1
+        }
+        return result
+    }
+}
