4bf3540c415a7aec7fb894e7ebc3d99542adf997
Andrey Yurin <andrey.yurin55@gmail.com>
2025-08-20 20:23:42 +0300

    Time: 9 ms (82.61%) | Memory: 65.5 MB (91.30%) - LeetSync

---
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.kt
@@ -0,0 +1,26 @@
+class Solution {
+    // Complexity:
+    // Time O(MN) and Space O(N) where M and N are the number
+    // of rows and columns in matrix, respectively.
+    fun countSquares(matrix: Array<IntArray>): Int {
+        var result = 0
+        // dp[c]@r:= the size of the largest one-square whose
+        // top-left corner is cell(r, c).
+        val dp = IntArray(matrix[0].size + 1) // base case: r=matrix.size
+
+        for (r in matrix.indices.reversed()) {
+            var tmp = 0 // dp[c+1]@r+1
+            for (c in matrix[r].indices.reversed()) {
+                val subResult =
+                    if (matrix[r][c] == 0) {
+                        0
+                    } else {
+                        1 + minOf(dp[c], dp[c + 1], tmp)
+                    }
+                result += subResult
+                dp[c] = subResult.also { tmp = dp[c] }
+            }
+        }
+        return result
+    }
+}
