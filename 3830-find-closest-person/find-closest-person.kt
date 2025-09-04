da3c666aa0caf3bd70c9f102221dbbd94206ebde
Andrey Yurin <andrey.yurin55@gmail.com>
2025-09-04 17:55:43 +0300

    Time: 1 ms (83.33%) | Memory: 40.7 MB (75.00%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun findClosest(x: Int, y: Int, z: Int): Int {
+        val p1 = abs(x-z)
+        val p2 = abs(y-z)
+    
+        return if (p1 < p2) {
+            1
+        } else if (p1 > p2) {
+            2
+        } else { 
+            0
+        }
+    }
+}
